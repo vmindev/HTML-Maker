396832b016b618157274824dc2377a063efd84a5
Varetoo <alvaromi2005@gmail.com>
2025-02-12 20:17:35 +0100

    Rama principal usando Python-Markdown. Eliminar archivos y módulos obsoletos relacionados con la conversión a HTML

--- html_maker.py
+++ html_maker.py
@@ -1,24 +1,10 @@
 import re
+import markdown
 from pathlib import Path
-from modules.headers import del_header, app_header
-from modules.lista import sep_lista, app_lista
-from modules.saltos_pagina import del_saltos, app_saltos
-from modules.bloque import sep_bloque, app_bloque
 
 ruta_texto:Path = Path(__file__).parent/"texto.txt"
 ruta_html_nuevo:Path = Path(__file__).parent/"documento.html"
 ruta_html_base:Path = Path(__file__).parent/"base.html"
-
-patrones_interiores:dict = {
-    "blockquote":r"\>(.*?)",        # Citado
-    "strong":r"\*\*(.*?)\*\*",      # Negrita
-    "em":r"\*(.*?)\*",              # Cursiva
-    "del":r"\~\~(.*?)\~\~",         # Tachada
-    "code":r"\`(.*?)\`",            # Formato de codigo
-    "a":r"\[(.*?)\]\((.*?)\)",      # Enlace
-    "img":r"\!\[(.*?)\]\((.*?)\)",  # Imagen
-}
-
 
 def leer(ruta:Path): # Lee el archivo de la ruta y devuelve el contenido en forma de lista
     datos:list = []
@@ -29,24 +15,6 @@ def leer(ruta:Path): # Lee el archivo de la ruta y devuelve el contenido en form
 
 def convert_html(texto_original:list): # Devuelve el texto formateado a html
     texto:list = texto_original.copy()
-    # ========== Separamos el texto en: texto sin el bloque concreto y diccionario/lista con indice y texto formateado ==========
-    # Saltos de pagina
-    sp_list:list = [int] # [indice1, indice2, ...] (indice=indice donde se encuentra originalmente el salto de página)
-    texto, sp_list = del_saltos(texto)
-    # Headers
-    headers_dict:dict = {int:str} # {indice: linea} (indice=indice original) (linea=linea formateada con html)
-    texto, headers_dict = del_header(texto)
-    # Listas
-    lista_dict:dict = {int:str} # {indice: linea} (indice=indice original) (linea=linea formateada con html)
-    texto, lista_dict = sep_lista(texto)
-    # Bloques de codigo
-    bloque_dict:dict = {int:list} # {indice: []} (indice=indice original) ([]=<pre>,linea, linea2, ...,</pre>, linea=<code>linea</code>)
-    texto, bloque_dict = sep_bloque(texto)
-    # ========== Combinamos el texto de nuevo ==========
-    texto = app_bloque(texto, bloque_dict) # Bloque de código
-    texto = app_lista(texto, lista_dict) # Listas
-    texto = app_header(texto, headers_dict) # Headers
-    texto = app_saltos(texto, sp_list) # Saltos de página
     
     return texto
 
