2bcecaa3949364bf46f375cd3455cc25635b6dd8
Varetoo <alvaromi2005@gmail.com>
2025-02-12 23:31:49 +0100

    Refactor y mejorar el formato de listas en la conversión a HTML

--- html_maker.py
+++ html_maker.py
@@ -8,9 +8,25 @@ ruta_html_base:Path = Path(__file__).parent/"base.html"
 
 # Almacenamos el contenido del archivo de texto
 with open(ruta_texto, "r", encoding="utf-8") as file:
-    contenido_texto:str = file.read()
+    contenido_texto:str = file.readlines()
+# Arreglamos el formato de las listas
+patron_lista = r"^(\s*)(-|\d+(?:\.\d+)*\.)\s+(.+)$"
+texto:str = ""
+temp_text = []
+for linea in contenido_texto:
+    linea_normal:bool = True
+    if match:=re.match(patron_lista, linea): # Esta linea es una lista
+        indent, tag, contenido = match.groups()
+        indent = len(indent)
+        if (indent%3 == 0) and (indent != 0): # La identacion está mal formateada (En espacios de 3)
+            linea_normal = False
+            indent = (indent//3)*4 # Corregimos el formato de identacion
+            espacios = " "*indent
+            texto += f"{espacios}{tag} {contenido}\n"
+            
+    if linea_normal: texto += linea
 # Convertimos el contenido en HTML
-html:str = markdown.markdown(contenido_texto, extensions=["extra"])
+html:str = markdown.markdown(texto, extensions=["extra"])
 html = html.split("\n")
 # Conseguimos el html base
 with open(ruta_html_base, "r", encoding="utf-8") as file:
