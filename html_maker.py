b319af85e3b88af5297b8d61efb53c9257b4f20b
Varetoo <alvaromi2005@gmail.com>
2025-02-12 22:56:45 +0100

    Refactor funciones de manejo de HTML: simplificar lectura y almacenamiento, mejorar estructura del archivo base

--- html_maker.py
+++ html_maker.py
@@ -1,43 +1,43 @@
-import re
-import markdown
 from pathlib import Path
 
-ruta_texto:Path = Path(__file__).parent/"texto.txt"
-ruta_html_nuevo:Path = Path(__file__).parent/"documento.html"
-ruta_html_base:Path = Path(__file__).parent/"base.html"
+ruta_texto = Path(__file__).parent/"texto.txt"
+ruta_html = Path(__file__).parent/"documento.html"
 
-def leer(ruta:Path): # Lee el archivo de la ruta y devuelve el contenido en forma de lista
-    datos:list = []
+def leer(ruta): # Lee el archivo de la ruta y devuelve el contenido
     with open(ruta, "r", encoding="utf-8") as file:
-        for linea in file:
-            datos.append(linea.strip("\n"))
+        datos = file.read()
     return datos
 
-def convert_html(texto_original:list): # Devuelve el texto formateado a html
-    texto:list = texto_original.copy()
+def convert_html(texto): # Devuelve el texto formateado a html
+    texto = texto.split("\n")
+    html = ""
+    for linea in texto:
+        if not linea:
+            print("Linea vacia")
+        elif (1+1):
+            pass
     
-    return texto
+    
+    return html
 
-def almacenar(ruta:Path, html_base:list, html_texto:list): # Almacena el contenido de 'texto_html' en un archivo html con una plantilla
-    # Patrón de búsqueda para </body>
-    patron_cierre_body = r"^\s*</body>\s*$"
-    # Buscamos el indice de </body> en la lista 'html'
-    for i, linea in enumerate(html_base):
-        if re.match(patron_cierre_body, linea):
-            index:int = i
-            break
-    # Introducimos el texto con formato html en la lista 'html'
-    for linea in html_texto:
-        html_base.insert(index, linea)
-        index += 1
-    # Añadimos un salto de pagina al final de cada linea
-    html_base = list(map(lambda linea: linea+"\n", html_base))
-    # Escribimos el el archivo el html
+def almacenar(ruta, texto_html): # Almacena el contenido de 'texto_html' en un archivo html con una plantilla
+    html = f'''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            {texto_html}
+        </body>
+        </html>
+    '''
     with open(ruta, "w", encoding="utf-8") as file:
-        list(map(lambda linea: file.write(linea), html_base))
+        file.write(html)
 
 if __name__=="__main__":
-    contenido_txt:list = leer(ruta_texto)
-    html_base:list = leer(ruta_html_base)
-    html_texto:list = convert_html(contenido_txt)
-    almacenar(ruta_html_nuevo, html_base, html_texto)
+    contenido_txt = leer(ruta_texto)
+    html = convert_html(contenido_txt)
+    almacenar(ruta_html, html)
